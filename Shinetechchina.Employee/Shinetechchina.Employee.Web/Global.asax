7b3a9f73d80d1dbf64e4e2ac3377c2dc8b79650f
vinniefeng <fcq121@163.com>
2017-02-17 14:49:25 +0800

    Create Project

---
+++ Shinetechchina.Employee/Shinetechchina.Employee.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shinetechchina.Employee.Web.MvcApplication" Language="C#" %>
---
+++ Shinetechchina.Employee/Shinetechchina.Employee.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shinetechchina.Employee.Web.Startup))]
+namespace Shinetechchina.Employee.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
