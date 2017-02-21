2ee0dbb38fe093f7e073e10b7d41ac50ec00e33d
vinniefeng <fcq121@163.com>
2017-02-21 15:15:46 +0800

    update test project

---
+++ Shinetechchina.Employee/Shinetechchina.Employee.Repository.Mock/RepositoryMockContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Shinetechchina.Employee.Core;
+using Shinetechchina.Employee.Repository.Shared;
+
+namespace Shinetechchina.Employee.Repository.Mock
+{
+    public class RepositoryMockContext : ContextBase
+    {
+        public Mock<IEmployeeRepository> mock = new Mock<IEmployeeRepository>();
+        public RepositoryMockContext()
+        {
+            SetUpMock();
+        }
+
+        public override Dictionary<Type, Object> GetRegister()
+        {
+            return new Dictionary<Type, Object>
+            {
+                [typeof(IEmployeeRepository)] = mock.Object,
+            };
+        }
+
+        private void SetUpMock()
+        {
+            mock.Setup(m => m.GetAllEmployee()).Returns(
+              (new List<EmployeeEntry>() {
+                    new EmployeeEntry { Email="Email", EmployeeID="EmployeeID", FirstName="FirstName1", Id=Guid.NewGuid(), LastName="LastName", Phone="Phone" },
+                    new EmployeeEntry { Email="Email", EmployeeID="EmployeeID", FirstName="FirstName2", Id=Guid.NewGuid(), LastName="LastName", Phone="Phone" },
+                    new EmployeeEntry { Email="Email", EmployeeID="EmployeeID", FirstName="FirstName3", Id=Guid.NewGuid(), LastName="LastName", Phone="Phone" },
+               }).AsEnumerable()
+               );
+            mock.Setup(m => m.GetEmployee(It.IsAny<string>())).Returns(
+                    new EmployeeEntry { Email = "Email", EmployeeID = "EmployeeID", FirstName = "FirstName1", Id = Guid.NewGuid(), LastName = "LastName", Phone = "Phone" }
+              );
+            mock.Setup(m => m.AddEmployee(It.IsAny<EmployeeEntry>())).Returns(1);
+            mock.Setup(m => m.DeleteEmployee(It.IsAny<string>())).Returns(1);
+            mock.Setup(m => m.UpdateEmployee(It.IsAny<EmployeeEntry>())).Returns(1);
+        }
+    }
+}
--- Shinetechchina.Employee/Shinetechchina.Employee.Repository.Test/EmployeeTest.cs
+++ Shinetechchina.Employee/Shinetechchina.Employee.Repository.Test/EmployeeTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Shinetechchina.Employee.Repository.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class EmployeeTest
     {
         [TestMethod]
         public void TestMethod1()
