a1ce0dd4d6630173c062a5229173c53f9a7114e0
vinniefeng <fcq121@163.com>
2017-02-21 15:39:31 +0800

    update moq

--- Shinetechchina.Employee/Shinetechchina.Employee.Web.Tests/Controllers/EmployeeControllerTests.cs
+++ Shinetechchina.Employee/Shinetechchina.Employee.Web.Tests/Controllers/EmployeeControllerTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shinetechchina.Employee.Web.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shinetechchina.Employee.Web.Controllers.Tests
 {
