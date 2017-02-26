0d7465b79ed6ba412d0bc8dcbf6ccfd30e3262c2
vinniefeng <fcq121@163.com>
2017-02-26 17:02:55 +0800

    add comment

--- Shinetechchina.Employee/Shinetechchina.Employee.Business.Shared/Contracts/IEmployeeMgr.cs
+++ Shinetechchina.Employee/Shinetechchina.Employee.Business.Shared/Contracts/IEmployeeMgr.cs
@@ -4,10 +4,38 @@ namespace Shinetechchina.Employee.Business.Shared
 {
     public interface IEmployeeMgr
     {
+        /// <summary>
+        /// get employee by employeeid
+        /// </summary>
+        /// <param name="id">employeeid</param>
+        /// <returns>employee model</returns>
         EmployeeModel GetEmployee(string id);
+
+        /// <summary>
+        /// get all employee with out paged
+        /// </summary>
+        /// <returns>employee model list</returns>
         IEnumerable<EmployeeModel> GetAllEmployee();
+
+        /// <summary>
+        /// update employee by employee id
+        /// </summary>
+        /// <param name="employee">employee model</param>
+        /// <returns>is update success </returns>
         bool UpdateEmployee(EmployeeModel employee);
+
+        /// <summary>
+        /// add a employee 
+        /// </summary>
+        /// <param name="employee">employee model</param>
+        /// <returns>is add success </returns>
         bool AddEmployee(EmployeeModel employee);
+
+        /// <summary>
+        /// delete an employee by id
+        /// </summary>
+        /// <param name="id">employee ID</param>
+        /// <returns>is success </returns>
         bool DeleteEmployee(string id);
     }
 }
--- Shinetechchina.Employee/Shinetechchina.Employee.Repository.Shared/Contracts/IEmployeeRepository.cs
+++ Shinetechchina.Employee/Shinetechchina.Employee.Repository.Shared/Contracts/IEmployeeRepository.cs
@@ -4,10 +4,38 @@ namespace Shinetechchina.Employee.Repository.Shared
 {
     public interface IEmployeeRepository
     {
+        /// <summary>
+        /// get an employee by employee id
+        /// </summary>
+        /// <param name="id">employee id </param>
+        /// <returns>employee entry</returns>
         EmployeeEntry GetEmployee(string id);
+
+        /// <summary>
+        /// get all employee without paged
+        /// </summary>
+        /// <returns>employee list</returns>
         IEnumerable<EmployeeEntry> GetAllEmployee();
+
+        /// <summary>
+        /// add an employee 
+        /// </summary>
+        /// <param name="employee">employee entry</param>
+        /// <returns>is success </returns>
         int AddEmployee(EmployeeEntry employee);
-        int DeleteEmployee(string employee);
+
+        /// <summary>
+        /// delete an employee 
+        /// </summary>
+        /// <param name="employee">emplouee id</param>
+        /// <returns>is success </returns>
+        int DeleteEmployee(string employeeID);
+
+        /// <summary>
+        /// update an employee 
+        /// </summary>
+        /// <param name="employee">emplouyee entry</param>
+        /// <returns>is success </returns>
         int UpdateEmployee(EmployeeEntry employee);
     }
 }
